154fca3e2d3ffed55fdab3ef144ee27585e0f808
Gadi Aleksandrowicz <gadial@gadial-desktop.(none)>
2010-01-19 19:28:12 +0200

    Small fix

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,7 +5,6 @@ class MainController < ApplicationController
     @partitions = []
     if params[:n] != nil and params[:n].to_i <= @max
       @partitions = display_partitions(params[:n].to_i)
-      STDERR.puts @partitions
     end
   end
 end
